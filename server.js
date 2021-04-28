806ecc0b4dcbfb544c9ceccad94fa3a2b484738c
Danciak <fatality1922@gmail.com>
2021-04-28 15:35:10 +0200

    add endpoints with mongoose

--- server.js
+++ server.js
@@ -3,24 +3,25 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const employeesRoutes = require('./routes/employees.routes');
-const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
+const departmentsRoutes = require('./routes/departments.routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false })); //mozliwe ze trzeba bedzie zmienic ten plik na wzor z modulu
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', employeesRoutes);
-app.use('/api', departmentsRoutes);
 app.use('/api', productsRoutes);
+app.use('/api', departmentsRoutes);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
-});
+})
 
-mongoose.connect('mongodb://localhost:27017/companyDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// connects our backend code with the database
+mongoose.connect('mongodb://localhost:27017/companyDB', { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
@@ -28,13 +29,6 @@ db.once('open', () => {
 });
 db.on('error', err => console.log('Error ' + err));
 
-app.use((req, res, next) => {
-  req.db = db;
-  next();
-});
-
-
 app.listen('8000', () => {
   console.log('Server is running on port: 8000');
 });
-
